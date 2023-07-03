59fc9a5968ac4204f057d7f1407b7104111b2438
IronWalrus68 <camsjobhunting@gmail.com>
2023-07-03 01:57:43 +0100

    server working

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+
+
+
+
+
+
+// keep this at the bottom
+app.listen(3000, ()  => {
+    console.log('listening on port 3000')
+})
