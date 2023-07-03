1a914e0a1837f09b0a30654f618d0ac7727393f0
IronWalrus68 <camsjobhunting@gmail.com>
2023-07-03 04:43:58 +0100

    make header, footer and nav bar

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+    const title = "Home 🏠"
+    res.render('home', {title:title})
 })
 
 
